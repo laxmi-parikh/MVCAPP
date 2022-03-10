e82097c0ece60b573f75ae1a18e281d8d3a8b9d2
laxmi-parikh <laxmi.parmar30@gmail.com>
2022-03-10 17:54:23 +0530

    https://github.com/laxmi-parikh/MVCAPP.git

---
+++ WebApplicationsample/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationsample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult details()
+        {
+            ViewBag.Message = "Your product details page.";
+
+            return View();
+        }
+
+    }
+}
